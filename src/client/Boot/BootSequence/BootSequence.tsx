1223e78cd2c93cf5bb6d4a0223090c5ec4d0d916
Lidcer <47243657+Lidcer@users.noreply.github.com>
2021-04-04 17:25:37 +0200

    Add files via upload

---
+++ src/client/Boot/BootSequence/BootSequence.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class BootSequence extends React.Component {
+  render() {
+    return (
+      <div className=''>
+        <div></div>
+      </div>
+    );
+  }
+}
